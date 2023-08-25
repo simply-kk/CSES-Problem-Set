c97bb890f08c9a548701b4e4126d31f056cec259
simply__kk <shhkhan33@gmail.com>
2023-08-25 09:46:53 +0530

    Introductory Problems

---
+++ Introductory Problems/1_Weird.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    long long n;
+    cin >> n;
+    while (true)
+    {
+        cout << n << " ";
+        if (n == 1)
+            break;
+        if (n % 2 == 0)
+            n = n / 2;
+        else
+            n = 3 * n + 1;
+    }
+    cout << "\n";
+    return 0;
+}
